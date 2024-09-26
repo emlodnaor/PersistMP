2dfcd31a87aaa5c57fb22a3f1044aeba67f8037e
emlodnaor <102614505+emlodnaor@users.noreply.github.com>
2024-09-26 22:45:02 +0200

    Update Client PersistMP.lua

--- PersistMP/Client/lua/ge/extensions/PersistMP.lua
+++ PersistMP/Client/lua/ge/extensions/PersistMP.lua
@@ -12,26 +12,26 @@ local M = {}
 
 local function onWorldReadyState(worldReadyState)
   if worldReadyState == 2 then
-	if M.PersistMPInfo ~= nil then
-		local vehName = M.PersistMPInfo.Vehicles[1].config.vcf.model
-		local vehConfigFile = M.PersistMPInfo.Vehicles[1].config.vcf.partConfigFilename
-		local pos = M.PersistMPInfo.Vehicles[1].positionRaw.pos
-		local rot = M.PersistMPInfo.Vehicles[1].positionRaw.rot
+	TriggerServerEvent("PersistMP_RequestStoredInfo", "")
+  end
+end
+
+function onPersistMP_GetAndApplyStoredInfo(json)
+	data = jsonDecode(json)
+	if data ~= nil then
+		local vehName = data.Vehicles[1].config.vcf.model
+		local vehConfigFile = data.Vehicles[1].config.vcf.partConfigFilename
+		local pos = data.Vehicles[1].positionRaw.pos
+		local rot = data.Vehicles[1].positionRaw.rot
 		core_vehicles.spawnNewVehicle(vehName, {config = vehConfigFile})
 		be:getPlayerVehicle(0):setPositionRotation( pos[1], pos[2], pos[3], rot[1], rot[2], rot[3], rot[4]) 
 	end
-  end
-  print(M)
 end
 
-function onPersistMPJoin(data)
-	M.PersistMPInfo = jsonDecode(data)
-end
-
-AddEventHandler("onPersistMPJoin", onPersistMPJoin) 
+AddEventHandler("onPersistMP_GetAndApplyStoredInfo", onPersistMP_GetAndApplyStoredInfo) 
 
 M.onInit = function() setExtensionUnloadMode(M, "manual") end
-M.onPersistMPJoin = onPersistMPJoin
+M.onPersistMP_GetAndApplyStoredInfo = onPersistMP_GetAndApplyStoredInfo
 M.onWorldReadyState = onWorldReadyState
 
 print("PersistMP Client loaded...")
